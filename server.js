ce4f9cdc30d25ba2258271c45dc282645c009794
Arshjot singh <singharshjot07@gmail.com>
2023-04-04 13:30:20 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require("express")
+const app=express();
+const HTTP_PORT = process.env.PORT || 8080;
+
+
+app.use(express.json());
+
+app.use(express.static("assets"));
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.get("/services",(req,res)=>{
+    res.sendFile(__dirname+"/services.html");
+})
+
+
+const onHttpStart=()=>{
+    console.log("Servers are running");
+}
+app.listen(HTTP_PORT, onHttpStart);
